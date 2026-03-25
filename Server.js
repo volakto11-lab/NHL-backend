e0017fe75e5521539b88f2f726506827562f3f9a
volakto11-lab <volak.to11@gmail.com>
2026-03-26 00:53:14 +0100

    Add root route to respond with backend status

--- Server.js
+++ Server.js
@@ -3,3 +3,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server běží na portu " + PORT);
 });
+app.get("/", (req, res) => {
+  res.send("Backend běží 🚀");
+});
