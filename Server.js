d2a3ed98134c1b7033ca6b5bfa24346c39ed42ee
volakto11-lab <volak.to11@gmail.com>
2026-03-26 01:10:11 +0100

    Refactor server initialization in Server.js

--- Server.js
+++ Server.js
@@ -1,8 +1,12 @@
+const express = require("express");
+const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Backend běží 🚀");
+});
 
 app.listen(PORT, () => {
   console.log("Server běží na portu " + PORT);
-});
-app.get("/", (req, res) => {
-  res.send("Backend běží 🚀");
 });
